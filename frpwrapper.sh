b205a147cb85979107d0e92ef03577b698585451
funnypro <funnypro@qq.com>
2019-04-23 22:35:08 +0800

    主脚本微调

--- frpwrapper.sh
+++ frpwrapper.sh
@@ -7,6 +7,13 @@
 type '[[' >/dev/null 2>&1 || alias '[['='['
 type '[[' >/dev/null 2>&1 || exit 1
 
+help(){
+    echo "${0##*/}" '<-f <frp execute file>> <"-o <frp options">>'
+}
+frp_execute_file_invalid(){
+    echo "frp execute file invalid"
+}
+
 while getopts 'f:o:' OPT; do
     case "${OPT}" in
     f)
@@ -16,20 +23,20 @@ while getopts 'f:o:' OPT; do
         frp_options="${OPTARG}"
     ;;
     *)
-        echo "${0##*/}" '<-f <frp execute file>> <-o <frp options>>'
+        help
         exit 1
     ;;
     esac
 done
 
 
-[[ -z "${frp_execute_file}" ]] && echo "${0##*/}" '<-f <frp execute file>> <"-o <frp options">>' && exit 1
+[[ -z "${frp_execute_file}" ]] && help && exit 1
 
 #不要问我为什么这里这么怪异，我的建议是问 sh
 #为了 sh 做出的牺牲
-[[ ! -s "${frp_execute_file}" ]] && echo "frp execute file invalid" && exit 1
-[[ ! -r "${frp_execute_file}" ]] && echo "frp execute file invalid" && exit 1
-[[ ! -x "${frp_execute_file}" ]] && echo "frp execute file invalid" && exit 1
+[[ ! -s "${frp_execute_file}" ]] && frp_execute_file_invalid && exit 1
+[[ ! -r "${frp_execute_file}" ]] && frp_execute_file_invalid && exit 1
+[[ ! -x "${frp_execute_file}" ]] && frp_execute_file_invalid && exit 1
 
 
 [[ -z "${frp_options}" ]] && exit 1 || eval exec "${frp_execute_file}" "${frp_options}" | cut -d ' ' -f 3-
